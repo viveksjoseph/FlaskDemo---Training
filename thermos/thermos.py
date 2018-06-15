83d368a2e04f5b88719e1602b9b3671eaa47cb26
Vivek SJ <viveksjoseph@Viveks-MacBook-Pro.local>
2018-06-15 15:07:00 -0700

    Flask - WTF, Views, Templates
    Flask - WTF
    Message Flashing/Secret Key
    Templates
    If, For, With
    Rendering form fields and errors
    CSRF Protection
    Macros
    Safe Protection
    Custom Validation - Flask-WTForm

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
 from datetime import datetime
+from forms import BookmarkForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = b'oy\xe6W\xe1\xddJ\xaa\x1f-$\xb6\xe0\x95d\xf3^\xc1\x10<]\xf4U\xf2'
@@ -11,36 +12,39 @@ class User:
         self.lastName = lastName
 
     def initials(self):
-        return self.firstName + " " +self.lastName
+        return self.firstName + " " + self.lastName
 
     def __str__(self):
-        return self.firstName + " " +self.lastName
+        return self.firstName + " " + self.lastName
 
-def store_bookmark(url):
+def store_bookmark(url, description):
     bookmarks.append(dict(
         url = url,
         user = "My Name",
+        description = description,
         date = datetime.utcnow()
     ))
+
+def new_bookmarks(num):
+    return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
 
 # Basic View
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', titles = ["Some passed", "Title passed", "Another passed"], 
-                                        user = User("SomeName1", "SomeName2"), 
-                                        alt_user = User("Name1", "Name2"),
-                                        third_user = User("AnotherName1", "AnotherName2"))
+    return render_template('index.html', new_bookmarks = new_bookmarks(5))
 
 # Bookmarks View
 @app.route('/add', methods = ['GET', 'POST'])
 def add():
-    if request.method == "POST":
-        url = request.form['url']
-        store_bookmark(url)
-        flash("Stored bookmark '{}'".format(url))
+    form = BookmarkForm()
+    if form.validate_on_submit():
+        url = form.url.data
+        description = form.description.data
+        store_bookmark(url, description)
+        flash("Stored bookmark '{}'".format(description))
         return redirect(url_for('index'))
-    return render_template('add.html')
+    return render_template('add.html', form = form)
 
 # 404 error page
 @app.errorhandler(404)
