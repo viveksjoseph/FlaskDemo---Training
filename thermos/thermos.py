f2349da0c3b2b5314e094dd40f52d5514fba4b79
Vivek SJ <viveksjoseph@viveks-mbp.lan>
2018-06-16 20:43:56 -0700

    Modify for SQL database
    Modify for SQL database

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,38 +1,28 @@
-from flask import Flask, render_template, url_for, request, redirect, flash
+import os
+from flask import Flask, render_template, url_for, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from forms import BookmarkForm
 
+#path to current python file
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = b'oy\xe6W\xe1\xddJ\xaa\x1f-$\xb6\xe0\x95d\xf3^\xc1\x10<]\xf4U\xf2'
-bookmarks = []
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-class User:
-    def __init__(self, firstName, lastName):
-        self.firstName = firstName
-        self.lastName = lastName
+import models
 
-    def initials(self):
-        return self.firstName + " " + self.lastName
-
-    def __str__(self):
-        return self.firstName + " " + self.lastName
-
-def store_bookmark(url, description):
-    bookmarks.append(dict(
-        url = url,
-        user = "My Name",
-        description = description,
-        date = datetime.utcnow()
-    ))
-
-def new_bookmarks(num):
-    return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
+def logged_in_user():
+    return models.User.query.filter_by(username='Name1').first()
 
 # Basic View
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', new_bookmarks = new_bookmarks(5))
+    return render_template('index.html', new_bookmarks = models.Bookmark.newest(5))
 
 # Bookmarks View
 @app.route('/add', methods = ['GET', 'POST'])
@@ -41,7 +31,9 @@ def add():
     if form.validate_on_submit():
         url = form.url.data
         description = form.description.data
-        store_bookmark(url, description)
+        bm = models.Bookmark(user = logged_in_user(), url = url, description = description)
+        db.session.add(bm)
+        db.session.commit()
         flash("Stored bookmark '{}'".format(description))
         return redirect(url_for('index'))
     return render_template('add.html', form = form)
@@ -55,6 +47,3 @@ def page_not_found(e):
 @app.errorhandler(500)
 def server_error(e):
     return render_template('500.hmtl'), 500
-
-if __name__ == '__main__':
-    app.run(debug=True)
