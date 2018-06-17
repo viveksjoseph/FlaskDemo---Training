94b52a36f45d19cd9a6dd36850ad5e4308799587
Vivek SJ <viveksjoseph@viveks-mbp.lan>
2018-06-16 20:39:09 -0700

    Fix import warning while compilation
    fix deprecation warnings

--- thermos/forms.py
+++ thermos/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
 from wtforms.fields import StringField
-from flask_wtf.html5 import URLField
+from wtforms.fields.html5 import URLField
 from wtforms.validators import DataRequired, url
 
 class BookmarkForm(Form):
