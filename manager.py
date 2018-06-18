4eed7768e81c31819e94d027d4fcb0bc5399bb98
Vivek SJ <viveksjoseph@viveks-mbp.lan>
2018-06-17 23:23:57 -0700

    Add default users
    Add default users

--- manager.py
+++ manager.py
@@ -7,8 +7,8 @@ manager = Manager(app)
 @manager.command
 def initdb():
     db.create_all()
-    db.session.add(User(username='Name1', email='name1@example.com'))
-    db.session.add(User(username='Name2', email='name2@example.com'))
+    db.session.add(User(username='Name1', email='name1@example.com', password="test"))
+    db.session.add(User(username='Name2', email='name2@example.com', password="test"))
     db.session.commit()
     print ('Initialzed the database')
 
