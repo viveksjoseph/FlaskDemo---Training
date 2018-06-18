01dd5d82c234802569052c7dfe1d4dbc2e3a7108
Vivek SJ <viveksjoseph@viveks-mbp.lan>
2018-06-17 23:27:07 -0700

    Add Login Form, Signup Form
    Add Login Form, Signup Form and validators

--- thermos/forms.py
+++ thermos/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField
+from wtforms.fields import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.fields.html5 import URLField
-from wtforms.validators import DataRequired, url
+from wtforms.validators import DataRequired, url, Length, Email, Regexp, EqualTo, ValidationError
+
+from thermos.models import User
 
 class BookmarkForm(FlaskForm):
     url = URLField('Enter URL for your bokmark: ', validators=[DataRequired(), url()])
@@ -18,3 +20,23 @@ class BookmarkForm(FlaskForm):
             self.description.data = self.url.data
 
         return True
+
+class LoginForm(FlaskForm):
+    username = StringField('Your Username: ', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
+
+class SignupForm(FlaskForm):
+    username = StringField('Username: ', validators=[DataRequired(), Length(3, 80), Regexp('^[A-Za-z0-9_]{3,}$', message='Usernames consists of numbers, letters and underscores')])
+    password = PasswordField('Password: ', validators=[DataRequired(), EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('Confirm password: ', validators=[DataRequired()])
+    email = StringField('Email: ', validators=[DataRequired(), Length(1,120), Email()])
+
+    def validate_email(self, email_field):
+        if (User.query.filter_by(email=email_field.data).first()):
+            raise ValidationError('There already is a user with this email address.')
+    
+    def validate_username(self, username_field):
+        if (User.query.filter_by(username=username_field.data).first()):
+            raise ValidationError('This username os already taken')
